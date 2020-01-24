da060091215e4c39f6b9c88aeeeeeed7dafd1bb5
dixroby <alestilodefe@gmail.com>
2020-01-24 01:28:50 +0100

    First commit : primer commit de inicio  de proyecto

---
+++ Alquiler.Web/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Alquiler.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alquiler.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Contract> Contracts { get; set; }
+        public DbSet<Lessee> Lessees { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Property> Properties { get; set; }
+        public DbSet<PropertyImage> PropertyImages { get; set; }
+        public DbSet<PropertyType> PropertyTypes { get; set; }
+
+
+        //Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
+    }
+
+}
---
+++ Alquiler.Web/Data/Entities/Property.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alquiler.Web.Data.Entities
+{
+    public class Property
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Barrio")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Neighborhood { get; set; }
+
+        [Display(Name = "la dirección")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Address { get; set; }
+
+        [Display(Name = "Precio")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Metros Cuadrados")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public int SquareMeters { get; set; }
+
+        [Display(Name = "Habitaciones")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public int Rooms { get; set; }
+
+        [Display(Name = "Stratum")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public int Stratum { get; set; }
+
+        [Display(Name = "Tiene estacionamiento?")]
+        public bool HasParkingLot { get; set; }
+
+        [Display(Name = "Está disponible?")]        
+        public bool IsAvailable { get; set; }
+
+        [Display(Name = "Observaciones")]
+        public string Remarks { get; set; }
+
+        public PropertyType PropertyType { get; set; }
+
+        public Owner Owner { get; set; }
+
+        public ICollection<PropertyImage> PropertyImages { get; set; }
+
+        public ICollection<Contract> Contracts { get; set; }
+
+    }
+}
